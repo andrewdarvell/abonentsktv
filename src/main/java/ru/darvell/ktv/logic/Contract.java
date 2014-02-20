758c3b8f7444d8a517b452a90adfe31e34d2b672
andrewdarvell <andrew@darvell.ru>
2014-02-20 16:00:13 +0400

    ПРодолжаем гибернейт

--- src/main/java/ru/darvell/ktv/logic/Contract.java
+++ src/main/java/ru/darvell/ktv/logic/Contract.java
@@ -1,6 +1,65 @@
 package ru.darvell.ktv.logic;
 
+import java.util.Date;
+
 public class Contract {
 
+	private Long id;
+	private Date dateStart;
+	private Date dateStop;
+	private Date dateCreate;
+	private String number;
+	private Abonent abonent = new Abonent();
 
+	public Contract() {
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDateStart() {
+		return dateStart;
+	}
+
+	public void setDateStart(Date dateStart) {
+		this.dateStart = dateStart;
+	}
+
+	public Date getDateStop() {
+		return dateStop;
+	}
+
+	public void setDateStop(Date dateStop) {
+		this.dateStop = dateStop;
+	}
+
+	public Date getDateCreate() {
+		return dateCreate;
+	}
+
+	public void setDateCreate(Date dateCreate) {
+		this.dateCreate = dateCreate;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public Abonent getAbonent() {
+		return abonent;
+	}
+
+	public void setAbonent(Abonent abonent) {
+		this.abonent = abonent;
+	}
 }
