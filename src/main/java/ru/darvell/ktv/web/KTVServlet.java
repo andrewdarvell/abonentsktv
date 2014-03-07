a6ce0454d0fb912f9fdfbf19fa7574b6c543b939
andrewdarvell <andrew@darvell.ru>
2014-03-07 14:22:18 +0400

    ПРодолжаем гибернейт. Немного сервлета.

--- src/main/java/ru/darvell/ktv/web/KTVServlet.java
+++ src/main/java/ru/darvell/ktv/web/KTVServlet.java
@@ -2,17 +2,12 @@ package ru.darvell.ktv.web;
 
 
 import org.apache.log4j.Logger;
-import org.hibernate.Session;
-import ru.darvell.ktv.dao.Factory;
-import ru.darvell.ktv.logic.Abonent;
-import ru.darvell.ktv.util.HibernateUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 
 public class KTVServlet extends HttpServlet {
 
@@ -24,41 +19,42 @@ public class KTVServlet extends HttpServlet {
 
 			response.setContentType("text/html; charset=utf-8");
 			PrintWriter out = response.getWriter();
-			out.println("<html><head><title>KTV servlet</title></head><body>");
+			out.println("<html><head><title>KTV servlet</title>");
+			out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"resources/css/page.css\">");
 
-			out.println("<table width=\"100%\" cellspacing=\"0\" cellpadding=\"5\">");
-			out.println("<tr>");
-			out.println("<td id=\"page_menu\" width=\"200\" valign=top>\"Левая колонка\"</td>");
-			out.println("<td id=\"page_body\" valign=top>\"Правая колонка\"");
+			out.println("</head>");
+			out.println("<body>");
+			out.println("<table id=\"page\">");
 
+			//Заголовок страницы
+			out.println("<tr id=\"page_header\">");
+			out.println("</tr>");
 
-			Session session = HibernateUtil.getSessionFactory().openSession();
-			Factory.getInstance().getAbonentDAO().setSession(session);
-			ArrayList<Abonent> abonents = (ArrayList<Abonent>) Factory.getInstance().getAbonentDAO().getAllAbonents();
-			out.println("<table border=\"1\" cellpadding=\"7\" >");
-			for (int i = 0; i < abonents.size(); i++) {
-				Abonent abonent = abonents.get(i);
-				out.println("<tr>");
-				out.println("<td>");
-				out.println(abonent.getId());
-				out.println("</td>");
-				out.println("<td>");
-				out.println(abonent.getLastName());
-				out.println("</td>");
-				out.println("<td>");
-				out.println(abonent.getFirstName());
-				out.println("</td>");
-				out.println("</tr>");
-			}
+			//Тело страницы
+			out.println("<tr id=\"page_body\">");
+
+			//Меню страницы
+			out.println("<td id=\"page_body_menu\">\"Левая колонка\"");
+
+			out.println("</td>");
+
+			//Контент
+			out.println("<td id=\"page_body_content\">\"Правая колонка\"");
+
 			out.println("</table>");
 
 			out.println("</td>");
 			out.println("</tr>");
+
+			out.println("</tr>");
+
+			out.println("<tr id=\"page_footer\">");
+			out.println("</tr>");
+
 			out.println("</table>");
 
 			out.println("</body></html>");
 
-			session.close();
 			out.flush();
 			out.close();
 
